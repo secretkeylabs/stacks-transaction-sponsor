aa74e41b0fd977aea0ef6ccf9bbacb974855f0df
Tim Man <timothyc.man@gmail.com>
2023-07-12 11:38:20 +0800

    fix: remove nft transfer specific validation logic and compare
    addressToString output for alex swap

--- src/validation.ts
+++ src/validation.ts
@@ -1,4 +1,4 @@
-import { PayloadType, ContractCallPayload, getAbi, addressToString, StacksTransaction } from '@stacks/transactions';
+import { PayloadType, ContractCallPayload, addressToString, StacksTransaction } from '@stacks/transactions';
 
 // rules for transaction sponsorship eligibility
 // customize this if you are forking this repo
@@ -8,27 +8,10 @@ export async function validateTransaction(transaction: StacksTransaction): Promi
   }
 
   const payload = transaction.payload as ContractCallPayload;
-
-  // check and limit to SIP-09 transfers calls
-  if (payload.contractAddress.hash160.toString() !== 'e685b016b3b6cd9ebf35f38e5ae29392e2acd51d') {
-    throw new Error('Transaction is not an ALEX swap contract call');
-  }
-
   const contractAddress = addressToString(payload.contractAddress);
-  const contractName = payload.contractName.content.toString();
-  const functionName = payload.functionName.content.toString();
-  const abi = await getAbi(contractAddress, contractName, 'mainnet');
-  const func = abi.functions.find((fn) => {
-    return fn.name === functionName;
-  });
 
-  if (func.args.length !== 3) {
-    throw new Error('Transaction is not a NFT transfer contract call');
-  }
-
-  // check against sip-09 interface for transfer
-  if (func.args[0].type !== 'uint128' || func.args[1].type !== 'principal' || func.args[2].type !== 'principal') {
-    throw new Error('Transaction is not a NFT transfer contract call');
+  if (contractAddress !== 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9') {
+    throw new Error('Transaction is not an ALEX swap contract address');
   }
 
   return true;
